5572d167d5ea17a2e8221d37157a3532384d808d
Maame Yaa Serwaa Bona-Mensa <mbonamensa@gmail.com>
2024-06-11 19:07:44 +0000

    fix footer positioning issue

--- src/Demo/components/Footer.jsx
+++ src/Demo/components/Footer.jsx
@@ -4,10 +4,12 @@ import { FaGithub } from "react-icons/fa";
 
 export default function Footer() {
     return (
-        <footer>
-            <p>Created by <strong>devMaame</strong></p> 
-            <a className="footer-icon-link" href="https://x.com/devMaame" target="_blank"><FaTwitter /></a> 
-            <a className="footer-icon-link" href="https://github.com/mbonamensa" target="_blank"><FaGithub /></a>
-        </footer>
+        <div className="footer-wrapper">
+            <footer>
+                <p>Created by <strong>devMaame</strong></p> 
+                <a className="footer-icon-link" href="https://x.com/devMaame" target="_blank"><FaTwitter /></a> 
+                <a className="footer-icon-link" href="https://github.com/mbonamensa" target="_blank"><FaGithub /></a>
+            </footer>
+        </div>
     )
 }
